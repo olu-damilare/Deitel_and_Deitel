3d2e7b274418d05d93306056854ba44ec1eba2fe
olu-damilare <56642018+olu-damilare@users.noreply.github.com>
2021-03-03 09:52:44 +0100

    Update DecimalToBinary.java

--- src/Exercise/DecimalToBinary.java
+++ src/Exercise/DecimalToBinary.java
@@ -5,12 +5,15 @@ public class DecimalToBinary {
     private String reversedBinary;
     private int binary;
 
-    public void convertEvenDecimalToBinary(int evenDecimalNumber) {
-        if (evenDecimalNumber % 2 == 0) {
+    public void convertEvenDecimalToBinary(int evenDecimalNumber)
+    {
+        if (evenDecimalNumber % 2 == 0)
+        {
             evenDecimalNumber += 1;
 
             reversedBinary = "";
-            while (evenDecimalNumber > 0) {
+            while (evenDecimalNumber > 0) 
+            {
                 reversedBinary += evenDecimalNumber % 2;
                 evenDecimalNumber /= 2;
             }
@@ -20,22 +23,27 @@ public class DecimalToBinary {
         }
     }
 
-        public void flipTheReverseOfTheBinary(){
+        public void flipTheReverseOfTheBinary()
+        {
             int integerOfReversedBinary = Integer.parseInt(reversedBinary);
 
             StringBinary = "";
-            while(integerOfReversedBinary > 0){
+            while(integerOfReversedBinary > 0)
+            {
                 StringBinary += integerOfReversedBinary % 10;
                 integerOfReversedBinary /= 10;
             }
 
         }
 
-    public void convertOddDecimalToBinary(int oddDecimalNumber){
+    public void convertOddDecimalToBinary(int oddDecimalNumber)
+    {
 
-        if (oddDecimalNumber % 2 == 1) {
+        if (oddDecimalNumber % 2 == 1) 
+        {
             reversedBinary = "";
-            while (oddDecimalNumber > 0) {
+            while (oddDecimalNumber > 0)
+            {
                 reversedBinary += oddDecimalNumber % 2;
                 oddDecimalNumber /= 2;
             }
@@ -44,7 +52,8 @@ public class DecimalToBinary {
         }
     }
 
-    public int convertDecimalToBinary(int decimalNumber){
+    public int convertDecimalToBinary(int decimalNumber)
+    {
         convertEvenDecimalToBinary(decimalNumber);
         convertOddDecimalToBinary(decimalNumber);
 
