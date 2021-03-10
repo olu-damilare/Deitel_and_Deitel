24bb95f20b0cf5c1586b527c14074fe3a52ae381
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-10 22:46:46 +0100

    Exercises

---
+++ src/chapterFive/FairTax.java
@@ -0,0 +1,21 @@
+package chapterFive;
+
+public class FairTax {
+
+    private double totalExpense = 0;
+
+    public void setExpense(double expense) {
+        if(expense > 0)
+        totalExpense += expense;
+    }
+
+    public double getTotalExpense() {
+        return totalExpense;
+    }
+
+    public double getCalculateTax() {
+        double rate = 0.30;
+
+        return rate * totalExpense;
+    }
+}
---
+++ test/tddClasses/FairTaxTest.java
@@ -0,0 +1,48 @@
+package tddClasses;
+
+import chapterFive.FairTax;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FairTaxTest {
+    FairTax fairTax;
+    @BeforeEach
+    void setUp(){
+        fairTax = new FairTax();
+    }
+
+    @AfterEach
+    void tearDown(){
+        fairTax = null;
+    }
+    @Test
+    void testForSingleExpenseValue(){
+        fairTax.setExpense(100.00);
+        assertEquals(100, fairTax.getTotalExpense());
+    }
+    @Test
+    void testForTotalOfMultipleExpenses(){
+        fairTax.setExpense(100.00);
+        fairTax.setExpense(200.00);
+        fairTax.setExpense(300.00);
+        assertEquals(600, fairTax.getTotalExpense());
+    }
+    @Test
+    void testForTaxRateOfTotalExpenses(){
+        fairTax.setExpense(100.00);
+        fairTax.setExpense(200.00);
+        fairTax.setExpense(300.00);
+
+        assertEquals(180, fairTax.getCalculateTax());
+    }
+    @Test
+    void testThatNegativeExpenseValueNotValid(){
+        fairTax.setExpense(-100.00);
+        fairTax.setExpense(200.00);
+        fairTax.setExpense(-300.00);
+        assertEquals(200, fairTax.getTotalExpense());
+    }
+}
