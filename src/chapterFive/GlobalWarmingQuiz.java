0eb7b8e6fe2074ab3020843a466c4de5097b5467
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-12 20:53:42 +0100

    Exercise

--- src/chapterFive/GlobalWarmingQuiz.java
+++ src/chapterFive/GlobalWarmingQuiz.java
@@ -2,11 +2,11 @@ package chapterFive;
 
 public class GlobalWarmingQuiz {
     private int totalScore = 0;
+    private String questionOne;
+    private String questionTwo;
 
     public void selectAnswer(String userSelection) {
-        userSelection = userSelection.toUpperCase();
-        boolean validSelection = userSelection.equals("A") || userSelection.equals("B") || userSelection.equals("C") || userSelection.equals("D");
-        if(validSelection)
+        if(userSelection.equals("A") || userSelection.equals("B") || userSelection.equals("C") || userSelection.equals("D"))
         ++totalScore;
     }
 
@@ -15,30 +15,30 @@ public class GlobalWarmingQuiz {
     }
 
     public String questionOne() {
-        String questionOne = """
-                Question 1:
-                                    
-                Which of the following does not trap heat?
-                A. Carbon dioxide
-                B. Nitrogen
-                C. Water vapor
-                D. Methane
-                """;
+        questionOne = """
+                    Question 1:
+                    
+                    Which of the following does not trap heat?
+                    A. Carbon dioxide
+                    B. Nitrogen
+                    C. Water vapor
+                    D. Methane
+                    """;
         return questionOne;
     }
 
 
     public String questionTwo(){
-        String questionTwo = """
-                Question 2:
-                How do scientists collect evidence about climate?
-                                    
-                A. Using remote sensing from space with satellites
-                B. By ground-based measurements of surface temperature,
-                   carbon dioxide concentration and sea level.
-                C. By collecting "proxy data" from tree rings, ice cores and historical records.
-                D. All of the above.
-                """;
+        questionTwo = """
+                    Question 2:
+                    How do scientists collect evidence about climate?
+                    
+                    A. Using remote sensing from space with satellites
+                    B. By ground-based measurements of surface temperature,
+                       carbon dioxide concentration and sea level.
+                    C. By collecting "proxy data" from tree rings, ice cores and historical records.
+                    D. All of the above.
+                    """;
         return questionTwo;
     }
 
@@ -88,6 +88,7 @@ public class GlobalWarmingQuiz {
 
 
     public String result() {
+
         if(totalScore == 5)
             return "Excellent";
         else if(totalScore == 4)
