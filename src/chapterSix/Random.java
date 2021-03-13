c7deee8b0ba5fb7028bd6e6081ed1dfa43e4b915
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-13 18:49:24 +0100

    Exercise

---
+++ src/chapterSix/Random.java
@@ -0,0 +1,22 @@
+package chapterSix;
+
+import java.security.SecureRandom;
+
+public class Random {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        for (int i = 0; i < 20; i++) {
+            int number = 1;
+            for (int j = 0; j < 8 ; j++) {
+                number = 6 + random.nextInt(17);
+                if(number == 6 || number == 10 || number == 14 || number == 18 ||  number == 22) {
+                    System.out.print(number + " ");
+                    break;
+                }
+            }
+
+
+        }
+
+    }
+}
