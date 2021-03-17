5b1cff84d2906001cce4b171de81e40748f11682
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-17 20:17:53 +0100

    Exercise

---
+++ test/tddClasses/RollDieTest.java
@@ -0,0 +1,27 @@
+package tddClasses;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RollDieTest {
+
+
+    @Test
+    void testThatMethodRandomlyGenerateFaceOfDie(){
+     assertNotEquals(RollDie.generateFace(), RollDie.generateFace());
+    }
+    @Test
+    void testThatGeneratedFaceIsCounted(){
+        RollDie.generateFace();
+        assertEquals(1, RollDie.countGeneratedFace());
+
+    }
+    @Test
+    void testToDisplayCountOfGeneratedFace(){
+        RollDie.generateFace();
+        int count = RollDie.countGeneratedFace();
+        assertEquals(1, RollDie.displayFaceCount(count));
+    }
+}
