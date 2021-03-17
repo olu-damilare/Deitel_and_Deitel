b257e3124a3581f82e0cb815521056b6c8069a5e
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-17 20:19:44 +0100

    Exercise

---
+++ src/tddClasses/RollDie.java
@@ -0,0 +1,25 @@
+package tddClasses;
+
+import java.security.SecureRandom;
+
+public class RollDie {
+    private static final SecureRandom random = new SecureRandom();
+    private static int generatedFace;
+    private static final  int[] die = new int[6];
+
+    public static int generateFace() {
+        generatedFace = 1 + random.nextInt(6);
+        return generatedFace;
+    }
+
+    public static int countGeneratedFace() {
+        die[generatedFace - 1]++;
+
+        return die[generatedFace - 1];
+    }
+    public static int displayFaceCount(int face){
+        return die[face - 1];
+    }
+
+
+}
