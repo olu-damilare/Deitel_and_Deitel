ce03f8f1684e3f4ac8a8a1d8e499eba84d82656a
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-17 11:37:23 +0100

    Exercise

--- src/chapterSix/GuessGame.java
+++ src/chapterSix/GuessGame.java
@@ -1,28 +1,37 @@
-package Exercise;
+package chapterSix;
 
 
-import java.util.Scanner;
+import java.security.SecureRandom;
 
 public class GuessGame {
+    private static int numberToGuess;
+    private static int guessCount;
 
-    public static void main(String[] args) {
-
-        int correctNumber = 25;
-
-        String message = "Enter an input to guess my number";
-        System.out.println(message);
-        Scanner input = new Scanner(System.in);
-        int userInput = input.nextInt();
-
-        while(userInput != correctNumber){
-            System.out.println("You're wrong, try again");
-            userInput = input.nextInt();
-        }
-        System.out.println("You win!!!");
+    public static int generateNumberToGuess() {
+        SecureRandom random = new SecureRandom();
+        numberToGuess = 1 + random.nextInt(1000);
+        return numberToGuess;
     }
 
-
-
-
-
+    public static String validateGuessedNumber(int number) {
+        if(number > numberToGuess){
+            guessCount++;
+            return "Too high, try again.";
+        }
+        else if(number < numberToGuess){
+            guessCount++;
+            return "Too low, try again.";
+        }
+        else
+            return "Congratulations. You guessed the number.";
+    }
+    public static String feedBack(){
+        if(guessCount < 10)
+            return "Either you know the secret or you got lucky!";
+        else if(guessCount > 10)
+            return "You should be able to do better! Why should it\n" +
+                    "take no more than 10 guesses? Well, with each “good guess”.";
+        else
+            return "Aha! You know the secret.";
+    }
 }
