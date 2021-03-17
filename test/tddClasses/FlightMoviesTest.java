b6688a564c99b04dd5f4e7e9d6d349d4c458916d
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-17 11:20:33 +0100

    Exercise

---
+++ test/tddClasses/FlightMoviesTest.java
@@ -0,0 +1,15 @@
+package tddClasses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlightMoviesTest {
+
+    @Test
+    void testThatSumOfLengthOfTwoMoviesEqualsFlightLength(){
+        int[] moviesLength = {150,220, 150,230,115};
+        int flightLength = 450;
+     assertTrue(FlightMovies.moviesLengthSumEqualsFlightLength(flightLength, moviesLength));
+    }
+}
