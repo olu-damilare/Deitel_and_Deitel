fbbeff4875d0daf657be85b4daf631f9e22ad43a
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-19 19:41:44 +0100

    Exercise

--- src/chapterFour/WorldPopulationGrowth.java
+++ src/chapterFour/WorldPopulationGrowth.java
@@ -2,22 +2,32 @@ package chapterFour;
 
 public class WorldPopulationGrowth {
     public void calculateWorldPopulationGrowth(){
-        long previousWorldPopulation = 7674000000l;
+        long initialWorldPopulation = 7674000000l;
+        long previousWordPopulation = initialWorldPopulation;
         double populationGrowthRate = 1.1;
         int year = 1;
-        long worldPopulation = previousWorldPopulation + (((long)populationGrowthRate * previousWorldPopulation)/100);
-        long difference =  worldPopulation - previousWorldPopulation;
+        long worldPopulation = initialWorldPopulation + (((long)populationGrowthRate * initialWorldPopulation)/100);
+        boolean isDouble = true;
+        int doublePopulation = 0;
+        long difference =  worldPopulation - initialWorldPopulation;
+
 
         System.out.println("Year\t" + "World Population\t\t" + "Population difference");
         System.out.println(year + "\t\t" + worldPopulation + "\t\t\t\t" + difference);
 
         while(year <= 75){
-            previousWorldPopulation = worldPopulation;
-            worldPopulation = previousWorldPopulation + (((long)populationGrowthRate * previousWorldPopulation)/100);
-            difference =  worldPopulation - previousWorldPopulation;
+            initialWorldPopulation = worldPopulation;
+            worldPopulation = initialWorldPopulation + (((long)populationGrowthRate * initialWorldPopulation)/100);
+            difference =  worldPopulation - initialWorldPopulation;
             System.out.println(year + "\t\t" + worldPopulation + "\t\t\t\t" + difference);
+            if(worldPopulation >= (2 * previousWordPopulation) && isDouble == true){
+                doublePopulation = year;
+                isDouble = false;
+            }
             year++;
 
         }
+
+        System.out.println("\n population doubled at year " + doublePopulation );
     }
 }
