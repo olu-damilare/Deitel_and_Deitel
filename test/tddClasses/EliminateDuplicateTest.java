040ed2f769401aa3abab00958cd685e1a525bb6f
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-09 22:09:09 +0100

    Exercise

---
+++ test/tddClasses/EliminateDuplicateTest.java
@@ -0,0 +1,20 @@
+package tddClasses;
+
+import Exercise.Array;
+import Exercise.DuplicateElements;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EliminateDuplicateTest {
+
+    @Test
+    void testThatDuplicateElementInArrayIsEliminated(){
+        int[] numbers = {0,0,1,1,2,2,3,3,4,4,5,6,7,7};
+        int[] secondNumbers = {-1,0,2,4,-3,-3,5};
+        assertEquals(14, numbers.length);
+
+       assertEquals(8, DuplicateElements.eliminateDuplicates(numbers));
+        assertEquals(6, DuplicateElements.eliminateDuplicates(secondNumbers));
+    }
+}
