c68473f6c83a899b0db0c51d5c406c3ffab104bc
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-18 05:05:57 +0100

    Exercise

--- src/turtleGraphics/Board.java
+++ src/turtleGraphics/Board.java
@@ -1,8 +1,23 @@
 package turtleGraphics;
 
 public class Board {
-    int[][] board = new int[2][];
+    int[][] board;
     public Board(int length, int breadth) {
-        board[0]
+        board = new int[length][breadth];
+    }
+
+    public void drawEastWard(int numberOfSteps) {
+
+    }
+
+    public String displayBoard() {
+        String pattern = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1)
+                    pattern += "*";
+            }
+        }
+        return pattern;
     }
 }
