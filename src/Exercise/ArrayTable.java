92fd382977af8550f5d0bb1bd5fa884ba23d0720
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-18 05:04:39 +0100

    Exercise

---
+++ src/Exercise/ArrayTable.java
@@ -0,0 +1,14 @@
+package Exercise;
+
+public class ArrayTable {
+    public static void main(String[] args) {
+        Array array = new Array();
+        int[][] table = {{1,2,3,4,5}, {1,0,1,1,1,2}, {4,5,6}, {7,8,9,10,11,12}};
+
+        array.displayIntArrayTable(table);
+
+        System.out.println("sum is " + array.getArraySum(table));
+
+
+    }
+}
