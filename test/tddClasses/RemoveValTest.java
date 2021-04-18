fd9e85a0d30088414b253d3a85d07e253828769b
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-18 05:00:21 +0100

    Exercise

---
+++ test/tddClasses/RemoveValTest.java
@@ -0,0 +1,15 @@
+package tddClasses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemoveValTest {
+
+    @Test
+    void testToRemoveValAndReturnNewLengthOfArray(){
+        int[] nums = {5,2,2,5,3};
+        int val = 5;
+        assertEquals(3, RemoveVal.remove(nums, val));
+    }
+}
