4f23c3432e2ebc9d1d511854c8cbc42717ffcf02
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-19 21:42:49 +0100

    Exercise

--- src/turtleGraphics/SketchPad.java
+++ src/turtleGraphics/SketchPad.java
@@ -1,27 +1,13 @@
 package turtleGraphics;
 
-public class Board {
-    int[][] board;
-    public Board(int length, int breadth) {
-        board = new int[length][breadth];
-    }
+public class SketchPad {
+    int[][] floor;
 
-    public void drawEastWard(int numberOfSteps) {
-
-    }
-
-    public String displayBoard() {
-        String pattern = "";
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1)
-                    pattern += "*";
-            }
-        }
-        return pattern;
+    public SketchPad(int length, int breadth) {
+        floor = new int[length][breadth];
     }
 
     public int[][] getFloor() {
-        return board;
+        return floor;
     }
 }
