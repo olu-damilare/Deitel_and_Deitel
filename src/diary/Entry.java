0afe3669e1707200b5cc72de203bd69cd820eaee
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-25 00:16:32 +0100

    Exercise

--- src/diary/Entry.java
+++ src/diary/Entry.java
@@ -1,9 +1,29 @@
 package diary;
 
+import java.time.LocalDateTime;
+
 public class Entry {
     private final String entryBody;
+    private String entryID;
+    private final LocalDateTime localDateTime;
 
     public Entry(String entryBody) {
         this.entryBody = entryBody;
+        localDateTime = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return  "Entry ID = " + entryID + '\n' +
+                "Time of entry creation = " + localDateTime + '\n' +
+                "Entry Body = " + entryBody;
+    }
+
+    public String getEntryID() {
+        return entryID;
+    }
+
+    public void assignEntryID(String entryID) {
+        this.entryID = entryID;
     }
 }
