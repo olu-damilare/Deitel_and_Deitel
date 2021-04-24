e580eafd51679fb2264c514292786c88ff7a5445
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-25 00:16:57 +0100

    Exercise

--- src/diary/Diary.java
+++ src/diary/Diary.java
@@ -5,12 +5,63 @@ import java.util.List;
 
 public class Diary {
     private List<Entry> entries = new ArrayList<>();
+    private static int entryIDCounter;
+    private String password;
 
     public void addEntry(Entry entry) {
+        String entryID = "DEID" + (++entryIDCounter);
+        entry.assignEntryID(entryID);
         entries.add(entry);
     }
 
     public int getTotalNumberOfEntries() {
         return entries.size();
+    }
+
+    public String getEntryID(Entry entry) {
+        for (Entry value : entries) {
+            if (value.equals(entry))
+                return value.getEntryID();
+        }
+        throw new IllegalArgumentException("Invalid entry");
+    }
+
+    public Entry getEntry(String entryID, String password) {
+        validatePassword(password);
+        Entry entry = findEntryByEntryID(entryID);
+        if (entry != null) return entry;
+        throw new IllegalArgumentException("Invalid entry ID");
+    }
+
+    private Entry findEntryByEntryID(String entryID) {
+        for (Entry entry : entries) {
+            if (entry.getEntryID().equals(entryID))
+                return entry;
+        }
+        return null;
+    }
+
+    private void validatePassword(String password) {
+        if(!(password.equals(this.password)))
+            throw new IllegalArgumentException("Invalid password");
+    }
+
+    public void deleteEntry(String password, String entryID) {
+        validatePassword(password);
+        Entry entry = findEntryByEntryID(entryID);
+        entries.remove(entry);
+    }
+
+    public List<Entry> getEntries(String password) {
+        validatePassword(password);
+        return entries;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
