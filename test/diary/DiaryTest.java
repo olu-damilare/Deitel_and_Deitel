f09cf31d04b4ad0ba3ab10e218ba2edca5ca6bc1
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-25 00:15:39 +0100

    Exercise

--- test/diary/DiaryTest.java
+++ test/diary/DiaryTest.java
@@ -2,6 +2,7 @@ package diary;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiaryTest {
@@ -13,5 +14,23 @@ public class DiaryTest {
         Entry entry = new Entry(entryBody);
         diary.addEntry(entry);
         assertEquals(1, diary.getTotalNumberOfEntries());
+        String entryID = diary.getEntryID(entry);
+        assertEquals(entry, diary.getEntry(entryID, entryID));
+
+        System.out.println(entry);
     }
-}
+
+    @Test
+    void testThatDiaryCanTakeMultipleEntries(){
+        Diary diary = new Diary();
+        String entryBody = "";
+        Entry entry = new Entry(entryBody);
+        diary.addEntry(entry);
+        Entry secondEntry = new Entry(entryBody);
+        diary.addEntry(secondEntry);
+        assertEquals(2, diary.getTotalNumberOfEntries());
+    }
+
+
+    }
+
