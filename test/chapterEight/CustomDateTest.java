a2c92e56ce2025381f2f3b38626cc40ac97609f6
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-25 02:18:43 +0100

    Exercise

--- test/chapterEight/CustomDateTest.java
+++ test/chapterEight/CustomDateTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CustomDateTest {
 
     @Test
-    void testThatDateCanIsNumerical(){
+    void testThatDateCanBeNumerical(){
         CustomDate date = new CustomDate(9, 27, 1985);
         assertEquals("09/27/1985", date.toString());
     }
@@ -22,4 +23,42 @@ public class CustomDateTest {
         }
 
     }
+    @Test
+    void testThatMonthIsValid(){
+        try {
+            CustomDate date = new CustomDate(50, 21, 1985);
+            assertEquals("00/21/1985", date.toString());
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    void testThatYearIsValid(){
+        try {
+            CustomDate date = new CustomDate(7, 21, 3000);
+            assertEquals("00/21/0000", date.toString());
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    @Test
+    void testThatDateCanBeCreatedUsingMonthName(){
+        CustomDate date = new CustomDate("June", 27, 1985);
+        assertEquals("June 27, 1985", date.getDate());
+    }
+    @Test
+    void testThatStringMonthMustBeValid(){
+        assertThrows(IllegalArgumentException.class, ()->  {CustomDate date = new CustomDate("jjjj", 27, 1985);});
+    }
+    @Test
+    void testThatDateCanCompriseDayAndYearOnly(){
+        CustomDate date = new CustomDate(120, 1985);
+        assertEquals("120 1985", date.getDayValueDate());
+    }
+    @Test
+    void testThatDayMustBeValidInDateThatCompriseDayAndYearOnly(){
+        assertThrows(IllegalArgumentException.class, ()->  {CustomDate date = new CustomDate(400, 1985);});
+    }
+
 }
