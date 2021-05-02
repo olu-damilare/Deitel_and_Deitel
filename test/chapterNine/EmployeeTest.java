29d69a59d7c1beb2c1b747264e4a8420abb6d53b
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-02 20:16:56 +0100

    Exercise'

---
+++ test/chapterNine/EmployeeTest.java
@@ -0,0 +1,48 @@
+package chapterNine;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+
+    @Test
+    void testThatCommissionEmployeeIsATypeOfEmployee(){
+        Calendar dateOfBirth = new GregorianCalendar(1984, Calendar.NOVEMBER, 23);
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Joe", "Doe", "123456789", 100000, 15, dateOfBirth);
+        assertEquals("Joe", commissionEmployee.getFirstName());
+        assertEquals("Doe", commissionEmployee.getLastName());
+        assertEquals("123456789", commissionEmployee.getSocialSecurityNumber());
+        assertEquals(100000, commissionEmployee.getGrossSales());
+        assertEquals(15, commissionEmployee.getCommissionRate());
+    }
+
+    @Test
+    void testThatCommissionEmployeeHasAnEarning(){
+        Calendar dateOfBirth = new GregorianCalendar(1984, Calendar.NOVEMBER, 23);
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Joe", "Doe", "123456789", 100000, 15, dateOfBirth);
+        assertEquals(15000, commissionEmployee.calculateEarning());
+    }
+
+    @Test
+    void testThatBasePlusCommissionEmployeeIsATypeOfCommissionEmployee(){
+        Calendar dateOfBirth = new GregorianCalendar(1984, Calendar.NOVEMBER, 23);
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee("Joe", "Doe", "123456789", 100000, 15, 50000, dateOfBirth);
+        assertEquals("Joe", employee.getFirstName());
+        assertEquals("Doe", employee.getLastName());
+        assertEquals("123456789", employee.getSocialSecurityNumber());
+        assertEquals(100000, employee.getGrossSales());
+        assertEquals(15, employee.getCommissionRate());
+        assertEquals(50000, employee.getBaseSalary());
+
+    }
+    @Test
+    void testThatBasePlusCommissionEmployeeHasAnEarning(){
+        Calendar dateOfBirth = new GregorianCalendar(1984, Calendar.NOVEMBER, 23);
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee("Joe", "Doe", "123456789", 100000, 15, 50000, dateOfBirth);
+        assertEquals(65000, employee.calculateEarning());
+    }
+
+}
---
+++ test/chapterNine/HourlyEmployeeTest.java
@@ -0,0 +1,51 @@
+package chapterNine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HourlyEmployeeTest {
+    HourlyEmployee employee;
+    Calendar dateOfBirth;
+
+    @BeforeEach
+    void setUp() {
+        dateOfBirth = new GregorianCalendar(1984, Calendar.NOVEMBER, 23);
+        employee = new HourlyEmployee("Joe", "Doe", "123456789", 40, 100, dateOfBirth);
+
+    }
+
+    @Test
+    void TestThatHourlyEmployeeIsATypeOfEmployee(){
+        assertEquals("Joe", employee.getFirstName());
+        assertEquals("Doe", employee.getLastName());
+        assertEquals("123456789", employee.getSocialSecurityNumber());
+        assertEquals(40, employee.getHoursWorked());
+        assertEquals(100, employee.getWagePerHour());
+    }
+    @Test
+    void TestThatHourlyEmployeeEarningCanBeCalculated(){
+        assertEquals(4000, employee.calculateEarning());
+    }
+    @Test
+    void TestThatHoursWorkedCanBeUpdated(){
+        assertEquals(4000, employee.calculateEarning());
+        employee.setHoursWorked(38);
+        assertEquals(3800, employee.calculateEarning());
+    }
+    @Test
+    void TestThatWageCanBeUpdated(){
+        assertEquals(4000, employee.calculateEarning());
+        employee.setWagePerHour(150);
+        assertEquals(6000, employee.calculateEarning());
+    }
+
+    @Test
+    void TestThatHourlyEmployeeCanEarnABonusForHoursGreaterThanForty(){
+        HourlyEmployee employee = new HourlyEmployee("John", "Doe", "123456789", 41, 100, dateOfBirth);
+        assertEquals(4150, employee.calculateEarning());
+    }
+}
