d5b8bc0558e285fbc5fb4363ea23531e0c55fedc
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-03 03:37:11 +0100

    Exercise

--- src/chapterTen/Payroll.java
+++ src/chapterTen/Payroll.java
@@ -1,5 +1,6 @@
 package chapterTen;
 
+import chapterNine.BasePlusCommissionEmployee;
 import chapterNine.Employee;
 
 import java.util.ArrayList;
@@ -8,21 +9,30 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Payroll {
-    List<Employee> employees = new ArrayList<>();
-    public Payroll(Employee... employees) {
-        this.employees.addAll(Arrays.asList(employees));
+    List<Payable> payable = new ArrayList<>();
+    public Payroll(Payable... payable) {
+        this.payable.addAll(Arrays.asList(payable));
     }
 
-    public double calculateEarning(Employee employee) {
+    public double calculatePayments(Payable payable) {
         LocalDateTime date = LocalDateTime.now();
-        if(employee.getMonthOfBirth() == date.getMonthValue() - 1)
-            return employee.calculateEarning() + 100;
-        else
-        return employee.calculateEarning();
+        if (payable instanceof BasePlusCommissionEmployee && ((Employee) payable).getMonthOfBirth() == date.getMonthValue() - 1)
+            return 100 + (1.10 * ((BasePlusCommissionEmployee) payable).getBaseSalary()) + (((BasePlusCommissionEmployee) payable).getCommissionRate()/100) * ((BasePlusCommissionEmployee) payable).getGrossSales();
+        if (payable instanceof BasePlusCommissionEmployee)
+            return (1.10 * ((BasePlusCommissionEmployee) payable).getBaseSalary()) + (((BasePlusCommissionEmployee) payable).getCommissionRate()/100) * ((BasePlusCommissionEmployee) payable).getGrossSales();
+        if (payable instanceof Employee)
+            if (((Employee) payable).getMonthOfBirth() == date.getMonthValue() - 1)
+                return payable.calculatePaymentAmount() + 100;
+
+        return payable.calculatePaymentAmount();
+
     }
 
-    public int getTotalNumberOfEmployees() {
-        return employees.size();
+    public int getTotalNumberOfPayable() {
+        return payable.size();
     }
 
+    public Payable calculatePaymentAmount(int i) {
+        return payable.get(i);
+    }
 }
