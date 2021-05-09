089be1cfe6dd9c12f8d80d281ab9e86b0172cfc8
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-09 06:40:08 +0100

    Exercise

---
+++ test/chapterFourteen/CheckProtectionTest.java
@@ -0,0 +1,12 @@
+package chapterFourteen;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckProtectionTest {
+
+    @Test
+    void testToImplementCheckProtection(){
+        CheckProtection checkProtection = new CheckProtection();
+        System.out.println(checkProtection.writeCheck("1,334.40")); ;
+    }
+}
