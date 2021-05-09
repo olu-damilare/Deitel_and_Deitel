2b75044244ebc6c3fc47b78a41c610ef0f48add1
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-09 01:10:45 +0100

    Exercise

--- src/chapterFour/Cryptography.java
+++ src/chapterFour/Cryptography.java
@@ -2,42 +2,56 @@ package chapterFour;
 
 public class Cryptography {
 
-    public String encode(int number) {
+    public int encrypt(int message) {
+        if(message < 1000 || message > 9999)
+            throw new IllegalArgumentException("message must be a four-digit number");
+        int encryptedMessage;
         int fourthValue = 0;
         int thirdValue = 0;
         int secondValue = 0;
-        int thirdValueHolder;
-        int fourthValueHolder;
-        String newValue = "";
-        if (number >= 1000 && number <= 9999) {
-            int counter = 1;
-            while (counter <= 4) {
-                if (number >= 1000) {
-                    fourthValue = ((number + 7) % 10);
-                    number /= 10;
-                } else if (number >= 100) {
-                    thirdValue = ((number + 7) % 10);
-                    number /= 10;
-                } else if (number >= 10) {
-                    secondValue = ((number + 7) % 10);
-                    number /= 10;
-                    number += 7;
-                }
-                counter++;
+
+        for (int i = 0; i < 5; i++) {
+            if (message >= 1000) {
+                fourthValue = ((message + 7) % 10);
+                message /= 10;
+            } else if (message >= 100) {
+                thirdValue = ((message + 7) % 10);
+                message /= 10;
+            } else if (message >= 10) {
+                secondValue = ((message + 7) % 10);
+                message /= 10;
+                message = (message + 7) % 10;
             }
-
-            thirdValueHolder = thirdValue;
-            thirdValue = number;
-            number = thirdValueHolder;
-
-            fourthValueHolder = fourthValue;
-            fourthValue = secondValue;
-            secondValue = fourthValueHolder;
-
-            newValue = number + "" + secondValue + "" + thirdValue + "" + fourthValue;
-
         }
-        return newValue;
+        encryptedMessage = (thirdValue * 1000) + (fourthValue * 100) + (message * 10) + secondValue;
+        return encryptedMessage;
+
+    }
+
+    public int decrypt(int message){
+        int decryptedMessage = 0;
+        int encryptedFourthValue = message % 10;
+        int encryptedThirdValue = (message % 100) / 10;
+        int encryptedSecondValue = (message / 100) % 10;
+        int encryptedFirstValue = message / 1000;
+
+        int decryptedFourthDigit = 0;
+        int decryptedThirdDigit = 0;
+        int decryptedSecondDigit = 0;
+        int decryptedFirstDigit = 0;
+
+        for (int j = 0; j < 10; j++) {
+            if(((j + 7) % 10) == encryptedFourthValue)
+                decryptedFourthDigit = j;
+            if(((j + 7) % 10) == encryptedThirdValue)
+                decryptedThirdDigit = j;
+            if(((j + 7) % 10) == encryptedSecondValue)
+                decryptedSecondDigit = j;
+            if(((j + 7) % 10) == encryptedFirstValue)
+                decryptedFirstDigit = j;
+        }
+        decryptedMessage = (decryptedThirdDigit * 1000) + (decryptedFourthDigit * 100) + ( decryptedFirstDigit * 10) + decryptedSecondDigit;
+        return decryptedMessage;
     }
 
 }
