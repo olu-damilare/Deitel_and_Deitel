96447cf4408a2ce582e6ca450feaa206b11eaafa
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-09 01:11:09 +0100

    Exercise

--- src/chapterFive/FacebookUserBaseGrowth.java
+++ src/chapterFive/FacebookUserBaseGrowth.java
@@ -6,28 +6,28 @@ public class FacebookUserBaseGrowth {
         double growthRate = 0.04;
         int month = 1;
         double newUsers = 0;
+        boolean firstTargetFound = false;
+        int firstTarget = 0;
+        int secondTarget = 0;
 
 
         while (true) {
             newUsers = users * Math.pow((1.0 + growthRate), month);
-            if (newUsers >= 1_500_000_000)
-                break;
-            users = (int) newUsers;
-            month++;
-        }
-        System.out.println("Facebook reached 1.5 billion users after " + month + " months");
+            if (newUsers >= 1_500_000_000 && !firstTargetFound) {
+                firstTarget = month;
+                firstTargetFound = true;
+            }
+            if (newUsers >= 2_000_000_000) {
+                secondTarget = month;
+               break;
+            }
 
-        newUsers = 0;
-        month = 1;
-        users = 1_000_000_000;
-        while (true) {
-            newUsers = users * Math.pow((1.0 + growthRate), month);
-            if (newUsers >= 2_000_000_000)
-                break;
-            users = (int) newUsers;
             month++;
         }
-        System.out.println("Facebook reached 2 billion users after " + month + " months");
+
+        System.out.println("Facebook reached 1.5 billion users after " + firstTarget + " months");
+
+        System.out.println("Facebook reached 2 billion users after " + secondTarget + " months");
         }
     }
 
