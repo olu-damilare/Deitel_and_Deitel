3f8055f39a77eab7b8ed14073322919c04e254b4
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-09 17:52:19 +0100

    Included the regex validation and separated functionalities into methods

--- src/chapterFourteen/CheckProtection.java
+++ src/chapterFourteen/CheckProtection.java
@@ -5,21 +5,25 @@ import java.util.List;
 
 public class CheckProtection {
     public String writeCheck(String checkValue) {
-        if (!checkValue.matches("(\\d[,]\\d*[.]\\d+ | \\d{0,3}[.]\\d{2}) "))
-            throw new IllegalArgumentException("Invalid value format. Check value should be in the format \"12,345.67\"");
+        validateCheckValueFormat(checkValue);
+        List<Character> charValues = getDigits(checkValue);
 
-        List<Character> charValues = new ArrayList<>();
-        for (int i = 0; i < checkValue.length(); i++) {
-            if(Character.isDigit(checkValue.charAt(i)))
-                charValues.add(checkValue.charAt(i));
+        StringBuilder writtenCheckValue = new StringBuilder();
+        for (Character charValue : charValues) {
+            writtenCheckValue.append(charValue);
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < charValues.size(); i++) {
-            sb.append(charValues.get(i));
-        }
-        double tempValue =  ((Integer.parseInt(sb.toString())) * 1.0)/100;
-        sb.delete(0, sb.length());
-        sb.append(tempValue);
+
+        double tempValue =  ((Integer.parseInt(writtenCheckValue.toString())) * 1.0)/100;
+        writtenCheckValue.delete(0, writtenCheckValue.length());
+        writtenCheckValue.append((String.format("%.2f",tempValue)));
+
+        insertAsterisksAndSeparatingComma(writtenCheckValue);
+
+        return writtenCheckValue.toString();
+
+    }
+
+    private void insertAsterisksAndSeparatingComma(StringBuilder sb) {
         int asteriskLimit =  5 - sb.substring(0, sb.length() - 3).length();
         for (int i = 0; i < asteriskLimit; i++) {
             sb.insert(0, '*');
@@ -27,13 +31,20 @@ public class CheckProtection {
         if(asteriskLimit < 2)
             sb.insert(2, ',');
         else sb.insert(0, '*');
-
-        return sb.toString();
-
     }
 
-    public static void main(String[] args) {
-        String value = "123";
-        System.out.println(value.matches("\\d[3]"));
+    private List<Character> getDigits(String checkValue) {
+        List<Character> charValues = new ArrayList<>();
+        for (int i = 0; i < checkValue.length(); i++) {
+            if(Character.isDigit(checkValue.charAt(i)))
+                charValues.add(checkValue.charAt(i));
+        }
+        return charValues;
     }
+
+    private void validateCheckValueFormat(String checkValue) {
+        if (!(checkValue.matches("\\d{1,2}[,]\\d{3}[.]\\d{1,2}") || checkValue.matches("\\d{0,3}[.]\\d{1,2}")))
+            throw new IllegalArgumentException("Invalid value format. Check value should be in the format \"12,345.67\"");
+    }
+
 }
