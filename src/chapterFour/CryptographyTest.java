1e05944804a807bb59cba76341fbef498fcd8377
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-10 15:55:32 +0100

    Exercise

--- src/chapterFour/CryptographyTest.java
+++ src/chapterFour/CryptographyTest.java
@@ -4,7 +4,7 @@ public class CryptographyTest {
     public static void main(String[] args) {
         Cryptography encrypt = new Cryptography();
 
-       System.out.println(encrypt.encrypt(9458));
+       System.out.println(encrypt.encrypt(3452));
        System.out.println(encrypt.decrypt(2561));
     }
 
