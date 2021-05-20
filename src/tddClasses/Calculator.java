3eab64187f40108b916aeb474b9514c6e18080fe
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-20 16:48:55 +0100

    Exercise

--- src/tddClasses/Calculator.java
+++ src/tddClasses/Calculator.java
@@ -2,24 +2,52 @@ package tddClasses;
 
 public class Calculator {
 
-    public int add(int numerator, int denominator) {
-        int sum = numerator + denominator;
-        return sum;
+    public static int add(int firstNumber, int secondNumber) {
+       return firstNumber + secondNumber;
     }
 
-    public double divide(int numerator, int denominator) {
-            if(denominator == 0)
-                return 0;
+    public static double divide(int numerator, int denominator) {
+        if(denominator == 0) return 0;
         return numerator /(denominator * 1.0);
     }
 
-    public int subtract(int firstNumber, int secondNumber) {
+    public static int subtract(int firstNumber, int secondNumber) {
         if(secondNumber > firstNumber)
             return secondNumber - firstNumber;
         return firstNumber - secondNumber;
     }
 
-    public int multiply(int firstNumber, int secondNumber) {
+    public static int multiply(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
+    }
+
+    public static int calculate(String expression) {
+        int value = 0;
+        String currentValue = "";
+        expression+="+";
+        char operator = '+';
+
+        for (int i = 0; i < expression.length(); i++) {
+            if(expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*' || expression.charAt(i) == '/') {
+                if(operator == '+') {
+                    value += Integer.parseInt(currentValue);
+                }
+                else if(operator == '-') {
+                    value -= Integer.parseInt(currentValue);
+                }
+                else if(operator == '*') {
+                    value *= Integer.parseInt(currentValue);
+                }
+                else if(operator == '/') {
+                    value /= Integer.parseInt(currentValue);
+                }
+                operator = expression.charAt(i);
+                currentValue = "";
+
+            }
+            else currentValue += expression.charAt(i);
+        }
+
+        return value;
     }
 }
