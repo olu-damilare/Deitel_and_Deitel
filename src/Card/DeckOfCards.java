25c81c7ed12da49ddb18805e648c2b4311e4d79f
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-20 21:32:52 +0100

    added features

---
+++ src/Card/DeckOfCards.java
@@ -0,0 +1,64 @@
+package Card;
+
+import chapterSeven.Card.Card;
+
+import java.util.Random;
+
+public class DeckOfCards {
+    private final Card[] cards;
+    private int lastPushedCardIndex = -1;
+
+    public DeckOfCards(int sizeOfDeck) {
+        cards = new Card[sizeOfDeck];
+    }
+
+    public int getSize() {
+        return cards.length;
+    }
+
+    public void push(Card card) {
+        if(isFull()) throw new StackOverflowException("Card deck is full");
+        cards[++lastPushedCardIndex] = card;
+    }
+
+    public int getNumberOfCardsInDeck() {
+        return lastPushedCardIndex + 1;
+    }
+
+    public Card peek() {
+        if(isEmpty()) throw new StackUnderflowException("Card deck is empty");
+        return cards[lastPushedCardIndex];
+    }
+
+    public Card pop() {
+        if(isEmpty()) throw new StackUnderflowException("Card deck is empty");
+        return cards[lastPushedCardIndex--];
+    }
+
+    public boolean isEmpty() {
+        return lastPushedCardIndex == -1;
+    }
+
+    public boolean isFull() {
+        return lastPushedCardIndex == getSize() - 1;
+    }
+
+    public void shuffle() {
+        int secondCardIndex = getSize() - 1;
+        int counter = 0;
+        Random randomizer = new Random();
+
+        while (secondCardIndex > counter) {
+            int firstCardIndex = randomizer.nextInt(getSize());
+            Card tempCard = cards[firstCardIndex];
+            cards[firstCardIndex] = cards[secondCardIndex];
+            cards[secondCardIndex] = tempCard;
+            secondCardIndex--;
+            counter++;
+        }
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+}
