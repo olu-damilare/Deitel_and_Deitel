0b9f72b8cb46c5d318b9bc858e67b5cc0baaac92
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-21 15:06:47 +0100

    tests

--- test/Card/PlayerTest.java
+++ test/Card/PlayerTest.java
@@ -1,10 +1,9 @@
 package Card;
 
-import chapterSeven.Card.Card;
 import org.junit.jupiter.api.Test;
 
-import static chapterSeven.Card.Faces.*;
-import static chapterSeven.Card.Suits.*;
+import static Card.Faces.*;
+import static Card.Suits.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -20,23 +19,61 @@ public class PlayerTest {
     }
 
     @Test
-    void testThatPlayerCanPlayCard(){
+    void testThatPlayerCanPlayCardToMatchingFace(){
         Player player = new Player("Joe");
+        DeckOfCards deck = new DeckOfCards(3);
         Card card = new Card(JACK, CLUB);
+        Card deckCard = new Card(JACK, HEARTS);
+        deck.push(deckCard);
         player.addCard(card);
         assertEquals(1, player.getNumberOfCards());
+        assertEquals(1, deck.getNumberOfCardsInDeck());
 
-        assertEquals(card, player.playCard(1));
+        player.playCard(1, deck);
         assertEquals(0, player.getNumberOfCards());
+        assertSame(card, deck.peek());
+        assertEquals(2, deck.getNumberOfCardsInDeck());
+    }
+
+    @Test
+    void testThatPlayerCanPlayCardToMatchingSuit(){
+        Player player = new Player("Joe");
+        DeckOfCards deck = new DeckOfCards(3);
+        Card card = new Card(JACK, CLUB);
+        Card deckCard = new Card(SEVEN, CLUB);
+        deck.push(deckCard);
+        player.addCard(card);
+        assertEquals(1, player.getNumberOfCards());
+        assertEquals(1, deck.getNumberOfCardsInDeck());
+
+        player.playCard(1, deck);
+        assertEquals(0, player.getNumberOfCards());
+        assertSame(card, deck.peek());
+        assertEquals(2, deck.getNumberOfCardsInDeck());
+    }
+
+    @Test
+    void testThatCardPlayedToNonMatchingFaceOrSuitThrowsInvalidCardException(){
+        Player player = new Player("Joe");
+        DeckOfCards deck = new DeckOfCards(3);
+        Card card = new Card(JACK, CLUB);
+        Card deckCard = new Card(SEVEN, HEARTS);
+        deck.push(deckCard);
+        player.addCard(card);
+        assertEquals(1, player.getNumberOfCards());
+        assertEquals(1, deck.getNumberOfCardsInDeck());
+
+        assertThrows(InvalidCardException.class, ()-> player.playCard(1, deck));
     }
 
     @Test
     void testThatWhenPlayerWithNoCardsPlayCard_throwsStackUnderFlowException(){
         Player player = new Player("Joe");
+        DeckOfCards deck = new DeckOfCards(3);
         assertEquals(0, player.getNumberOfCards());
         assertTrue(player.hasNoCard());
 
-        assertThrows(StackUnderflowException.class, ()-> player.playCard(1));
+        assertThrows(StackUnderflowException.class, ()-> player.playCard(1, deck));
     }
 
 }
