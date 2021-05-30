eda0ad6f57ead283428e0dfd59ec88768e6cdf39
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-30 15:51:57 +0100

    Exercise'

---
+++ src/Exercise/MergeAndSortLists.java
@@ -0,0 +1,45 @@
+package Exercise;
+
+
+import java.util.Arrays;
+
+public class MergeAndSortLists {
+
+
+ public static int[] mergeSortedLists(int[] firstList, int[] secondList){
+     if(firstList == null && secondList == null) return new int[]{};
+     else if((secondList == null || secondList.length == 0) && firstList != null) return firstList;
+     else if(firstList == null || firstList.length == 0) return secondList;
+
+     int[] newList = new int[firstList.length + secondList.length];
+
+     int firstListCounter = 0;
+     int secondListCounter = 0;
+     int newListCounter = 0;
+
+    while(firstListCounter < firstList.length && secondListCounter < secondList.length){
+        if(firstList[firstListCounter] > secondList[secondListCounter]){
+            newList[newListCounter] = secondList[secondListCounter];
+            secondListCounter++;
+        }
+        else{
+            newList[newListCounter] = firstList[firstListCounter];
+            firstListCounter++;
+        }
+        newListCounter++;
+    }
+
+    if(firstListCounter < firstList.length){
+        for (int i = firstListCounter; i < firstList.length; i++) {
+            newList[secondList.length + i] = firstList[i];
+        }
+    }else if(secondListCounter < secondList.length){
+        for (int i = secondListCounter; i < secondList.length; i++) {
+            newList[firstList.length + i] = secondList[i];
+        }
+    }
+    return newList;
+
+    }
+
+}
