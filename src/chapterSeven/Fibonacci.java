29d8619080edbda5a1a79c6629a790a166f67d15
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-09 17:53:12 +0100

    exercise

--- src/chapterSeven/Fibonacci.java
+++ src/chapterSeven/Fibonacci.java
@@ -1,15 +1,15 @@
 package chapterSeven;
 
-public class Fibonnaci {
-    public static int calculateFibonnaci(int n) {
+public class Fibonacci {
+    public static int calculateFibonacci(int n) {
         int firstNumber = 0;
         int secondNumber = 1;
-        int fibonnaciNumber = 0;
+        int FibonacciNumber = 0;
         for (int i = 0; i < n; i++) {
-            fibonnaciNumber = firstNumber + secondNumber;
+            FibonacciNumber = firstNumber + secondNumber;
             firstNumber = secondNumber;
-            secondNumber = fibonnaciNumber;
+            secondNumber = FibonacciNumber;
         }
-        return fibonnaciNumber;
+        return FibonacciNumber;
     }
 }
