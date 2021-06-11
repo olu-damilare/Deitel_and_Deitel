ba4b0c969fc2995b285de147deca645084a935e5
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-11 10:46:46 +0100

    added features

--- src/Exercise/KataMain.java
+++ src/Exercise/KataMain.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class KataMain {
     public static void main(String[] args) {
 
-        System.out.print("Enter a number to obtain its factors: ");
-        Scanner input = new Scanner(System.in);
-        int userInput = input.nextInt();
+//        System.out.print("Enter a number to obtain its factors: ");
+//        Scanner input = new Scanner(System.in);
+//        int userInput = input.nextInt();
+//
+//       String factors = Kata.calculateFactors(userInput);
+//
+//        System.out.println("The factors of " + userInput + " are " + factors);
 
-       String factors = Kata.calculateFactors(userInput);
 
-        System.out.println("The factors of " + userInput + " are " + factors);
     }
 }
