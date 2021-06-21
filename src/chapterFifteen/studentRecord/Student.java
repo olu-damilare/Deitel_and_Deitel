481fd99f2f1e6e1d09998efdc1839187ddaa1f50
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-21 13:47:01 +0100

    exercise

---
+++ src/chapterFifteen/studentRecord/Student.java
@@ -0,0 +1,35 @@
+package chapterFifteen.studentRecord;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    public Student(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+}
