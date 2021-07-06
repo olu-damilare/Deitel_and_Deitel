93faa66db5e1aecb707328a971c949c484ef3b72
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-06 10:09:10 +0100

    test

---
+++ test/chapterEighteen/ArrayMinValueTest.java
@@ -0,0 +1,15 @@
+package chapterEighteen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayMinValueTest {
+
+    @Test
+    void testToFindTheMinimumNumberInArray(){
+        ArrayMinValue determinant = new ArrayMinValue();
+        int[] nums = {4,2,5,6,4,3,1};
+        assertEquals(1, determinant.findRecursiveMinimum(nums));
+    }
+}
