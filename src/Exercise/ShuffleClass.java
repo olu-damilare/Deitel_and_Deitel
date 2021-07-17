67c2636ae50416999d2c0d12a043d95ca7320eed
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-17 01:12:52 +0100

    exercise

--- src/Exercise/ShuffleClass.java
+++ src/Exercise/ShuffleClass.java
@@ -1,6 +1,7 @@
 package Exercise;
 
-import java.util.Arrays;
+
+import java.util.Scanner;
 
 public class ShuffleClass {
 
@@ -27,9 +28,24 @@ public class ShuffleClass {
         return newCollection;
     }
 
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            int i = scan.nextInt();
+
+            // Write your code here.
+            double d = scan.nextDouble();
+            String s = scan.nextLine();
 
 
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
         }
+    }
 
-}
+
+
+
