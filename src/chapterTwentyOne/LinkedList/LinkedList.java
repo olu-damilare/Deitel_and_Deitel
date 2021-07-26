a17537454435d9b338ededf84649e1650479f5da
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-26 18:03:05 +0100

    exercise

---
+++ src/chapterTwentyOne/LinkedList/LinkedList.java
@@ -0,0 +1,31 @@
+package chapterTwentyOne.LinkedList;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+
+    public void addFromFront(T data) {
+        if (head == null) {
+            head = tail =  new Node<T>(data);
+        }else{
+            head = new Node<T>(null, data, head);
+        }
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFromBack(T data) {
+        if (head == null) {
+            head = tail =  new Node<T>(data);
+        }
+    }
+
+
+}
