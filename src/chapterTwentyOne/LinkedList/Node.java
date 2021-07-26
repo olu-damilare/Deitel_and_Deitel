463a6898148cadd90d9f7ea3486556d0705608a2
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-26 18:02:26 +0100

    exercise

---
+++ src/chapterTwentyOne/LinkedList/Node.java
@@ -0,0 +1,24 @@
+package chapterTwentyOne.LinkedList;
+
+public class Node<T> {
+    Node<T> previousNode;
+    Node<T> nextNode;
+    T data;
+
+    public Node(Node<T> previousNode, T data, Node<T> nextNode) {
+        this.previousNode = previousNode;
+        this.nextNode = nextNode;
+        this.data = data;
+    }
+
+    public Node(T data) {
+        this(null, data, null);
+    }
+
+    @Override
+    public String toString() {
+        return  "previousNode = " + previousNode +
+                "\nnextNode = " + nextNode +
+                "\ndata = " + data;
+    }
+}
