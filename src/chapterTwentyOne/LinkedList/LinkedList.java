db81d9f674c98061110a81911f3603ae92fbbc0a
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-27 12:18:32 +0100

    exercise

--- src/chapterTwentyOne/LinkedList/LinkedList.java
+++ src/chapterTwentyOne/LinkedList/LinkedList.java
@@ -25,10 +25,49 @@ public class LinkedList<T> {
         if (head == null) {
             head = tail =  new Node<T>(data);
         }else{
-            tail = new Node<T>(null, data, head);
-
+            tail = new Node<T>(tail, data, null);
         }
     }
 
+    public T removeFromFront(){
+        if(isEmpty()) throw new NullPointerException("List is empty");
+        T removedData = head.data;
+        if(head == tail) {
+            head = tail = null;
+        }else {
+            head = head.nextNode;
+        }
 
+        return removedData;
+    }
+
+
+    public T removeFromEnd() {
+        if(isEmpty()) throw new NullPointerException("List is empty");
+        T removedData = tail.data;
+        if(head == tail) {
+            head = tail = null;
+        }else {
+            tail = tail.previousNode;
+        }
+
+        return removedData;
+    }
+
+    public void updateNode(Node<T> node, T data) {
+        if (isEmpty()) throw new NullPointerException("List is empty");
+        if (head == tail) {
+            if (head == node) {
+                head.data = data;
+            }
+        } else {
+            Node<T> temp = head;
+            while (temp.nextNode != null) {
+                if (temp.nextNode == node) {
+                    temp.nextNode.data = data;
+                    return;
+                }
+            }
+        }
+    }
 }
