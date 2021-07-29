ff2832ace57ef8dfd1660a528784b0ea25a9b71c
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-29 15:17:22 +0100

    exercise

--- src/chapterTwentyOne/LinkedList/LinkedList.java
+++ src/chapterTwentyOne/LinkedList/LinkedList.java
@@ -1,19 +1,23 @@
 package chapterTwentyOne.LinkedList;
 
-public class LinkedList<T> {
+import javax.naming.directory.InvalidAttributeIdentifierException;
+
+
+public class LinkedList<T>{
     private Node<T> head;
     private Node<T> tail;
+    private Node<T> pointer;
 
-
-    public void addFromFront(T data) {
-        if (head == null) {
+    public Node<T> addFromFront(T data) {
+        if (isEmpty()) {
             head = tail =  new Node<T>(data);
+            head.previousNode = head.nextNode = tail;
+            pointer = tail;
         }else{
-            head = new Node<T>(null, data, head);
+            head = new Node<T>(tail, data, head);
+            tail.nextNode = head;
+            head.nextNode.previousNode = head;
         }
-    }
-
-    public Node<T> getHead() {
         return head;
     }
 
@@ -21,12 +25,16 @@ public class LinkedList<T> {
         return head == null;
     }
 
-    public void addFromBack(T data) {
-        if (head == null) {
+    public Node<T> addFromBack(T data) {
+        if (isEmpty()) {
             head = tail =  new Node<T>(data);
+            head.nextNode = head.previousNode = tail;
+            pointer = tail;
         }else{
-            tail = new Node<T>(tail, data, null);
+            tail = tail.nextNode = new Node<T>(tail, data, head);
+            head.previousNode = tail;
         }
+        return tail;
     }
 
     public T removeFromFront(){
@@ -36,6 +44,7 @@ public class LinkedList<T> {
             head = tail = null;
         }else {
             head = head.nextNode;
+            tail.nextNode = head;
         }
 
         return removedData;
@@ -49,25 +58,113 @@ public class LinkedList<T> {
             head = tail = null;
         }else {
             tail = tail.previousNode;
+            head.previousNode = tail;
+            tail.nextNode = head;
         }
 
         return removedData;
     }
 
-    public void updateNode(Node<T> node, T data) {
+    public void updateNode(Node<T> node, T data) throws InvalidAttributeIdentifierException {
         if (isEmpty()) throw new NullPointerException("List is empty");
-        if (head == tail) {
-            if (head == node) {
-                head.data = data;
-            }
-        } else {
+        if (head == tail && head == node) {
+            head.data = data;
+        }else if(head != tail){
             Node<T> temp = head;
-            while (temp.nextNode != null) {
+            while (temp.nextNode != head) {
                 if (temp.nextNode == node) {
                     temp.nextNode.data = data;
                     return;
                 }
+                temp = temp.nextNode;
             }
+        }else throw new InvalidAttributeIdentifierException("Invalid node");
+    }
+
+    public boolean isPresent(T data){
+        if(isEmpty()) throw new NullPointerException("List is empty");
+        if(head == tail)
+            return head.data == data;
+        Node<T> temp = head;
+        while(temp.nextNode != head){
+            if (temp.nextNode.data == data) return true;
+            temp = temp.nextNode;
+        }
+        return false;
+    }
+
+    public void removeNode(Node<T> node) throws InvalidAttributeIdentifierException {
+        if(isEmpty()) throw new NullPointerException("List is empty");
+        if(head == tail){
+            if(head == node){
+                head = tail = null;
+            }else throw new InvalidAttributeIdentifierException("Invalid node");
+        }
+        Node<T> temp = head;
+
+        do {
+            if(temp == node){
+                temp.previousNode.nextNode = temp.nextNode;
+                temp.nextNode.previousNode = temp.previousNode;
+                break;
+            }
+            temp = temp.nextNode;
+        } while (temp != head);
+    }
+
+    public void insertAfter(Node<T> node, T data) throws InvalidAttributeIdentifierException {
+        if(isEmpty()) throw new InvalidAttributeIdentifierException("Invalid node");
+        Node<T> temp = head;
+        do{
+            if(temp == node){
+                Node<T> newNode = new Node<T>(temp, data, temp.nextNode);
+                temp.nextNode = newNode;
+                temp.nextNode.previousNode = newNode;
+                return;
+            }
+            temp = temp.nextNode;
+        }while(temp != head);
+    }
+
+    @Override
+    public String toString() {
+        if(isEmpty()) return "[]";
+        String list = "[";
+
+        Node<T> temp = head;
+        do {
+            list += temp.data;
+
+                if (temp != tail){
+                    list += ", ";
+                }
+
+            temp = temp.nextNode;
+        } while (temp != head);
+
+        list += "]";
+        return list;
+    }
+
+    public boolean hasNext(){
+        return pointer != tail;
+    }
+
+    public T next(){
+        T temp = pointer.data;
+        pointer = pointer.nextNode;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.addFromFront(10);
+        list.addFromFront(2);
+        list.addFromFront(5);
+        list.addFromFront(8);
+
+        while(list.hasNext()){
+            System.out.println(list.next());
         }
     }
 }
