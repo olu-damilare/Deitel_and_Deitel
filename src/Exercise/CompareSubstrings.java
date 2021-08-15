a98a42e1f399d22ae0e39d02b9a3ca0ab15535c9
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-15 03:36:16 +0100

    exercise

---
+++ src/Exercise/CompareSubstrings.java
@@ -0,0 +1,31 @@
+package Exercise;
+
+public class CompareSubstrings {
+
+        public static String getSmallestAndLargest(String s, int k) {
+            String smallest = "";
+            String largest = "";
+
+            // Complete the function
+            // 'smallest' must be the lexicographically smallest substring of length 'k'
+            // 'largest' must be the lexicographically largest substring of length 'k'
+            smallest = largest = s.substring(0, k);
+            for(int i = 1; i < s.length(); i++){
+                if(i + k > s.length()) break;
+                String temp = s.substring(i, i + k);
+                if(temp.compareTo(smallest) < 0)
+                    smallest = temp;
+                else if(temp.compareTo(largest) > 0)
+                    largest = temp;
+
+            }
+
+            return smallest + "\n" + largest;
+        }
+
+
+        public static void main(String[] args) {
+            System.out.println(getSmallestAndLargest("ZASKFDLklhfsdfsDLJFSJGIHEKHIPEINNNFIGHKkjgksfgjrotyotoyjtkjkLJOIOEHEKHKKDJGKFGJkfjhglfhjtrhkjfkhjnfglhkjflgjhtrljhfljhfgljhfgljhfgljhtrklyjhtrkjhfgkljhfgjhfljhtrljlfjhfgljhfglkjhflyjtljtrlyjhtryjtrtykhrktherktjhtrkyjhkujhtykhtryhrthHKLJHLHRLHTLRHLKHTRLKHLHRLHLKHLKHKLHLKHLHKLHKHJKHKJHKJHJKHKHJKHKHHLHLHLHKHKJHKJKKHKHKHKHKHHKHKHKHKHkhktryhtlhtklhtrkyhtrkyhtrkjyhtrkyhrekthtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkrtkyhtrklyhjrOEOHKDHFksdhfklHLHKHLHKKJHJHKGKLHLHJLJHLHLHLHLHHLHLHLHH", 240));
+        }
+    }
+
