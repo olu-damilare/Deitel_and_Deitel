1101e086490b52f1e1c49cc7f9cbdbcf1c1b9a8f
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-15 03:33:22 +0100

    exercise

--- src/chapterTwentyOne/supermarket/Supermarket.java
+++ src/chapterTwentyOne/supermarket/Supermarket.java
@@ -1,7 +1,28 @@
 package chapterTwentyOne.supermarket;
 
-public class SupermarketSimulation {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
+public class Supermarket {
+    Queue<Customer> line;
 
+    public Supermarket() {
+        line = new LinkedList<Customer>();
+    }
 
+    public void scheduleArrival(Customer customer){
+        Random randomizer = new Random();
+        int arrivalTime = 1 + randomizer.nextInt(4);
+        customer.setArrivalTime(arrivalTime);
+        line.offer(customer);
+    }
+
+    public void serviceCustomer() throws InterruptedException {
+        if(!line.isEmpty()) {
+            System.out.println("Customer is being serviced");
+            Customer nextCustomer = line.poll();
+            Thread.sleep(nextCustomer.getArrivalTime());
+        }
+    }
 }
