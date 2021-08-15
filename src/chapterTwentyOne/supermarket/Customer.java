933156bf035d2eca8fa031775906d20a666fa292
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-15 03:33:58 +0100

    exercise

--- src/chapterTwentyOne/supermarket/Customer.java
+++ src/chapterTwentyOne/supermarket/Customer.java
@@ -3,4 +3,21 @@ package chapterTwentyOne.supermarket;
 public class Customer {
     private int arrivalTime;
     private int serviceTime;
+
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setServiceTime(int serviceTime) {
+        this.serviceTime = serviceTime;
+    }
 }
