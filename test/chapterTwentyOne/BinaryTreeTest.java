1855fee2399ad62211a8e961b85c7bd5335e98c4
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-16 23:20:10 +0100

    test

---
+++ test/chapterTwentyOne/BinaryTreeTest.java
@@ -0,0 +1,13 @@
+package chapterTwentyOne;
+
+import org.junit.jupiter.api.Test;
+
+public class BinaryTreeTest {
+
+    @Test
+    void testToInsertWithDuplicate(){
+        TreeNode<Integer> node = new TreeNode<Integer>(10);
+        Tree<Integer> tree = new Tree<Integer>();
+
+    }
+}
