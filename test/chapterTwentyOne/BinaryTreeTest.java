c9a546609e83f67be7fc2684301cac2510059009
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-17 01:01:38 +0100

    test

--- test/chapterTwentyOne/BinaryTreeTest.java
+++ test/chapterTwentyOne/BinaryTreeTest.java
@@ -2,12 +2,25 @@ package chapterTwentyOne;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BinaryTreeTest {
 
     @Test
     void testToInsertWithDuplicate(){
-        TreeNode<Integer> node = new TreeNode<Integer>(10);
         Tree<Integer> tree = new Tree<Integer>();
+        Random random = new Random();
+        int value =0;
+        for (int i = 0; i < 10; i++) {
+            value = random.nextInt(100);
+            System.out.print(value + " ");
+            tree.insert(value);
+        }
+        System.out.println();
+        assertTrue(tree.contains(value));
+
 
     }
 }
