839de4b63fd5ebadf354ca08202092e5969739d8
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-17 01:02:26 +0100

    exercise

--- src/chapterTwentyOne/Tree.java
+++ src/chapterTwentyOne/Tree.java
@@ -3,7 +3,7 @@ package chapterTwentyOne;
 public class Tree<T extends Comparable<T>> {
     private TreeNode<T> root;
 
-    public void insertNode(T value){
+    public void insert(T value){
         if(root == null) root = new TreeNode<T>(value);
         else root.insert(value);
     }
@@ -14,7 +14,7 @@ public class Tree<T extends Comparable<T>> {
 
     private void preorderHelper(TreeNode<T> node) {
         if (node == null) return;
-        System.out.println(node.data + " ");
+        System.out.print(node.data + " ");
         preorderHelper(node.leftNode);
         preorderHelper(node.rightNode);
 
@@ -27,7 +27,7 @@ public class Tree<T extends Comparable<T>> {
     private void inorderHelper(TreeNode<T> node) {
         if(node == null) return;
         inorderHelper(node.leftNode);
-        System.out.println(node.data + " ");
+        System.out.print(node.data + " ");
         inorderHelper(node.rightNode);
     }
 
@@ -42,5 +42,21 @@ public class Tree<T extends Comparable<T>> {
         System.out.println(node.data + " ");
 
     }
+
+    public boolean contains(T value){
+       return containHelper(root, value);
+    }
+
+    private boolean containHelper(TreeNode<T> node, T value){
+        if(node == null) return false;
+        if(node.data == value) return true;
+         boolean exists = containHelper(node.leftNode, value);
+        if(exists) return true;
+        return containHelper(node.rightNode, value);
+    }
+
+
+
+
 
 }
