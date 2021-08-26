c357aa11dca1e4988a6056c7e717da2b739e7613
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-26 06:13:03 +0100

    exercise

--- src/chapterTwentyOne/supermarket/Supermarket.java
+++ src/chapterTwentyOne/supermarket/Supermarket.java
@@ -1,28 +1,46 @@
 package chapterTwentyOne.supermarket;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
 public class Supermarket {
-    Queue<Customer> line;
 
-    public Supermarket() {
-        line = new LinkedList<Customer>();
-    }
+    ArrayList<Integer> customerQueue = new ArrayList<Integer>();
+    private int minutesPassed = 0;
+    private int timeLimit = 720;
+    private int waitingLimit = 4;
+    private int maximumQueueLength = 0;
 
-    public void scheduleArrival(Customer customer){
-        Random randomizer = new Random();
-        int arrivalTime = 1 + randomizer.nextInt(4);
-        customer.setArrivalTime(arrivalTime);
-        line.offer(customer);
-    }
+    public void serviceCustomer(){
+        while(minutesPassed <= timeLimit) {
+            int waitingTime = generateTime(waitingLimit);
+            customerQueue.add(waitingTime);
+            int serviceTime = generateTime(waitingLimit);
+            customerQueue.set(0, customerQueue.get(0) - serviceTime);
+            minutesPassed += serviceTime;
 
-    public void serviceCustomer() throws InterruptedException {
-        if(!line.isEmpty()) {
-            System.out.println("Customer is being serviced");
-            Customer nextCustomer = line.poll();
-            Thread.sleep(nextCustomer.getArrivalTime());
+            if(customerQueue.size() > maximumQueueLength){
+                maximumQueueLength = customerQueue.size();
+            }
+
+            if(customerQueue.get(0) <= 0){
+                customerQueue.remove(0);
+            }
         }
+        System.out.println(customerQueue);
+        System.out.println(maximumQueueLength);
+
+    }
+
+    private int generateTime(int boundary) {
+        Random random = new Random();
+        return 1 + random.nextInt(boundary);
+    }
+
+    public static void main(String[] args) {
+        Supermarket supermarket = new Supermarket();
+        supermarket.serviceCustomer();
     }
 }
